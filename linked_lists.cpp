129a716f153d052c7bd1b3e630c2346e3ef79752
dheerajpille <dheerajpille@gmail.com>
2017-12-12 13:48:39 -0500

    Added 2.5

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 struct Node {
-    int data = 0;
+    int data = 1;
     Node* next = nullptr;
 };
 
@@ -76,17 +76,57 @@ void deleteMiddleNode(Node* head) {
     delete nextNode;
 }
 
+// 2.4
 Node* partition(Node* head, int i) {
+    Node* left = nullptr;
+    Node* right = nullptr;
+    while (head) {
+        if (head->data < i) {
+            if (left == nullptr) {
+                left = head;
+            }
+            left->next = head->next;
+            print(left);
+        }
+        head = head->next;
+    }
+    return left;
+}
 
+// 2.5
+int pow(int b, int e) {
+    if (e == 0) {
+        return 1;
+    }
+    if (e == 1) {
+        return b;
+    }
+    return b * pow(b, e-1);
+}
+
+int sumLists(Node* head1, Node* head2) {
+    int value1 = 0, value2= 0;
+    int digits = 0;
+    while (head1) {
+        value1 += head1->data * pow(10, digits);
+        digits++;
+        head1 = head1->next;
+    }
+    digits = 0;
+    while (head2) {
+        value2 += head2->data * pow(10, digits);
+        digits++;
+        head2 = head2->next;
+    }
+    return value1+value2;
 }
 
 int main(void) {
     Node* head = new Node;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 1; i < 3; i++) {
         insert(head, i);
     }
     print(head);
-    deleteMiddleNode(head);
-    print(head);
+    cout << sumLists(head, head) << endl;
     return 0;
 }
