25bd8d8e6f2ca6779182c4fbdc78c81e8ad4ac4c
dheerajpille <dheerajpille@gmail.com>
2017-12-18 05:25:31 -0500

    Added 16.2

--- moderate.cpp
+++ moderate.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+// 16.1
 int numberSwapper(int& x, int& y) {
     x = x + y;
     y = x - y;
     x = x - y;
 }
 
+// 16.2
+int wordFrequencies(string word, vector<string> book) {
+    int counter = 0;
+    string check = "";
+    for (auto c : word) {
+        tolower(c);
+    }
+    for (auto words : book) {
+        check = "";
+        for (auto c : words) {
+            check += tolower(c);
+        }
+        if (word == check) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
 int main(void) {
+    vector<string> book = {"lOl", "I", "LOl"};
+    string word = "lol";
+    cout << wordFrequencies(word, book) << endl;
     return 0;
 }
