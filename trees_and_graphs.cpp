1c0d045371246ed50ccae50f4fada558e996a534
unknown <dheerajpille@gmail.com>
2017-12-21 06:50:37 -0500

    Added 4.2

--- trees_and_graphs.cpp
+++ trees_and_graphs.cpp
@@ -18,6 +18,34 @@ void insertRight(TreeNode* &root, TreeNode* right) {
     root->right = right;
 }
 
+void print(TreeNode* root)
+{
+    if (root == nullptr)
+        return;
+    print(root->left);
+    cout << root->val << " ";
+    print(root->right);
+}
+
+// 4.2
+TreeNode* minimalTree(vector<int> vec, int low, int high) {
+    if (low > high) {
+        return nullptr;
+    }
+    int mid = (low+high)/2;
+    TreeNode* newNode = new TreeNode(vec[mid]);
+    newNode->left = minimalTree(vec, low, mid-1);
+    newNode->right = minimalTree(vec, mid+1, high);
+    return newNode;
+}
+
+TreeNode* minimalTree(vector<int> vec) {
+    if (vec.size() == 0) {
+        return nullptr;
+    }
+    return minimalTree(vec, 0, vec.size()-1);
+}
+
 // 4.5
 bool validateBST(TreeNode* root, int min, int max) {
     if (root == nullptr) {
@@ -41,6 +69,9 @@ int main(void) {
     insertLeft(c, f);
     insertLeft(a, b);
     insertRight(a, c);
-    cout << validateBST(a, -2e9, 2e9) << endl;
+    //cout << validateBST(a, -2e9, 2e9) << endl;
+    vector<int> arr = {1,2,3,4,5,6,7,8};
+    TreeNode* root = minimalTree(arr);
+    print(root);
     return 0;
 }
